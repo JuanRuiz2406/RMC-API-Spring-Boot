fb5104af2dfae1de23c123792414eb519a4c24e2
dvperga <44186637+dvperga@users.noreply.github.com>
2020-11-25 23:23:41 -0600

    Inicio del proyecto

---
+++ ReportsMyCity/src/main/java/com/api/ReportsMyCity/ReportsMyCityApplication.java
@@ -0,0 +1,13 @@
+package com.api.ReportsMyCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsMyCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsMyCityApplication.class, args);
+	}
+
+}
---
+++ ReportsMyCity/src/test/java/com/api/ReportsMyCity/ReportsMyCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ReportsMyCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsMyCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
