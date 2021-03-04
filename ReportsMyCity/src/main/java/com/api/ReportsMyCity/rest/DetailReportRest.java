98998e0101a187d6ebaf0e05c59503e599aa8650
JuanRuiz2406 <44301111+JuanRuiz2406@users.noreply.github.com>
2021-03-04 00:17:21 -0600

    Funcionalidad de reporte especifico

--- ReportsMyCity/src/main/java/com/api/ReportsMyCity/rest/DetailReportRest.java
+++ ReportsMyCity/src/main/java/com/api/ReportsMyCity/rest/DetailReportRest.java
@@ -4,6 +4,7 @@ import com.api.ReportsMyCity.entity.DetailReport;
 import com.api.ReportsMyCity.entity.Report;
 import com.api.ReportsMyCity.exceptions.ResourceNotFoundException;
 import com.api.ReportsMyCity.repository.DetailReportRepository;
+import com.api.ReportsMyCity.repository.ReportRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,9 +16,11 @@ import java.util.Optional;
 public class DetailReportRest {
 
     private final DetailReportRepository detailReportRepository;
+    private ReportRest reportRest;
 
-    public DetailReportRest(DetailReportRepository detailReportRepository) {
+    public DetailReportRest(DetailReportRepository detailReportRepository, ReportRest reportRest) {
         this.detailReportRepository = detailReportRepository;
+        this.reportRest = reportRest;
     }
 
     @GetMapping
@@ -40,12 +43,13 @@ public class DetailReportRest {
 
     }
 
-    @GetMapping(value = "/byReport/{report}")
-    public ResponseEntity<List<DetailReport>> getByReport(@PathVariable("report") Report report){
+    @GetMapping(value = "/byReport/{reportId}")
+    public ResponseEntity<List<DetailReport>> getByReportId(@PathVariable("reportId") int reportId){
 
+        Report report = reportRest.getById(reportId);
         List<DetailReport> detailsByReport = detailReportRepository.findByReport(report);
         if(detailsByReport.isEmpty()){
-            throw new ResourceNotFoundException("No existen detalles");
+            return ResponseEntity.ok(detailsByReport);
         }
         return ResponseEntity.ok(detailsByReport);
     }
