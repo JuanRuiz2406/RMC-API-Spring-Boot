d5b3a50e6398e47b326be3825a6b42418be2f5e0
Marco Alvarado <marcotrabajos2013@gmail.com>
2021-04-20 14:59:31 -0600

    Email VerificationCode
    Se agrega la dependencia spring-boot-starter-thymeleaf
    Se agrega carpeta resources/templates
    Se agrega la plantilla del correo de código de verificación
    Se agrega la clase generadora de un código de verificación
    Resultado: Se envía exitosamente un correo con plantilla que tiene un código de verificación único

--- ReportsMyCity/src/main/java/com/api/ReportsMyCity/email/SendingEmailAplication.java
+++ ReportsMyCity/src/main/java/com/api/ReportsMyCity/email/SendingEmailAplication.java
@@ -2,26 +2,39 @@ package com.api.ReportsMyCity.email;
 
 import com.api.ReportsMyCity.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 @Service
 public class SendingEmailAplication {
     private JavaMailSender javaMailSender;
-
+    private TemplateEngine templateEngine;
     @Autowired
-    public void SendingEmailAplication(JavaMailSender javaMailSender){
+    public void SendingEmailAplication(JavaMailSender javaMailSender,TemplateEngine templateEngine){
+        this.templateEngine = templateEngine;
         this.javaMailSender = javaMailSender;
-    }
-    public void sendNotification(User user) throws MailException {
 
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setTo(user.getEmail());
-        mail.setFrom("reportsmycity@gmail.com");
-        mail.setSubject("Restablece tu contraseña");
-        mail.setText("Hola " + user.getName() + " " + user.getLastname() + " Hemos sido notificados " +
-                "que deseas restablecer tu contraseña, para esto debes de seguir los siguientes pasos: ");
-        javaMailSender.send(mail);
+    }
+    public String sendNotification(User user) throws MessagingException {
+        Context context = new Context();
+        context.setVariable("user",user);
+        String process = templateEngine.process("verificationCode", context);
+        javax.mail.internet.MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+        helper.setSubject("Código de Seguridad de ReportsMyCity");
+        helper.setText(process, true);
+        helper.setTo(user.getEmail());
+        javaMailSender.send(mimeMessage);
+        return "Sent";
+
     }
 }
--- ReportsMyCity/src/main/java/com/api/ReportsMyCity/rest/MailSenderRest.java
+++ ReportsMyCity/src/main/java/com/api/ReportsMyCity/rest/MailSenderRest.java
@@ -1,11 +1,14 @@
 package com.api.ReportsMyCity.rest;
 
+import com.api.ReportsMyCity.email.RandomString;
 import com.api.ReportsMyCity.email.SendingEmailAplication;
 import com.api.ReportsMyCity.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
 
 @RestController
 public class MailSenderRest {
@@ -14,16 +17,18 @@ public class MailSenderRest {
     private SendingEmailAplication sendingEmailAplication;
 
     @RequestMapping("/sendEmail")
-    public String Send(){
+    public String Send() throws MessagingException {
+
+        RandomString random = new RandomString();
+
+
         User user =  new User();
         user.setName("Marco");
-        user.setLastname("Alvarado");
+        user.setLastname(random.nextString());
         user.setEmail("alvaradomarco2011@gmail.com");
-        try {
-            sendingEmailAplication.sendNotification(user);
-        }catch(MailException e){
-            System.out.println(e.getMessage());
-        }
+        sendingEmailAplication.sendNotification(user);
         return "enviado";
     }
+
+
 }
