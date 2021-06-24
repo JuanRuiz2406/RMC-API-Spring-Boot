5c68610dfab699d3f1c19997ada8a4e2281cd136
JuanRuiz2406 <44301111+JuanRuiz2406@users.noreply.github.com>
2021-06-24 15:13:38 -0600

    no mostrar reportes pendientes en mobile + mensaje al no tener muni que responda por alguna ciudad

--- src/main/java/com/api/ReportsMyCity/repository/ReportRepository.java
+++ src/main/java/com/api/ReportsMyCity/repository/ReportRepository.java
@@ -21,7 +21,7 @@ public interface ReportRepository extends JpaRepository<Report, Integer> {
     List<Report> findByStateOrderByIdDesc(String state);
 
     @Transactional (readOnly = true)
-    List<Report> findByStateNotAndStateNotAndPrivacyOrderByIdDesc(String state, String state2, String privacy);
+    List<Report> findByStateNotAndStateNotAndStateNotAndPrivacyOrderByIdDesc(String state, String state2, String state3, String privacy);
 
     @Transactional (readOnly = true)
     List<Report> findAllByOrderByIdDesc();
--- src/main/java/com/api/ReportsMyCity/rest/ReportRest.java
+++ src/main/java/com/api/ReportsMyCity/rest/ReportRest.java
@@ -1,11 +1,11 @@
 package com.api.ReportsMyCity.rest;
 
-import com.api.ReportsMyCity.entity.*;
-import com.api.ReportsMyCity.exceptions.ApiOkException;
-import com.api.ReportsMyCity.exceptions.ApiUnproccessableEntityException;
+import com.api.ReportsMyCity.entity.Coordenates;
+import com.api.ReportsMyCity.entity.Municipality;
+import com.api.ReportsMyCity.entity.Report;
+import com.api.ReportsMyCity.entity.User;
 import com.api.ReportsMyCity.exceptions.ResourceNotFoundException;
 import com.api.ReportsMyCity.repository.ReportRepository;
-import com.api.ReportsMyCity.rest.UserRest;
 import com.api.ReportsMyCity.security.dto.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -47,38 +47,39 @@ public class ReportRest {
     @CrossOrigin
     @GetMapping(value = "/byPublicPrivacyAndVisibleState")
     public ResponseEntity<List<Report>> getByPublicPrivacyAndVisibleState() {
-        List<Report> reportsToShow = reportRepository.findByStateNotAndStateNotAndPrivacyOrderByIdDesc("Rechazado", "Eliminado", "Público");
+        List<Report> reportsToShow = reportRepository.findByStateNotAndStateNotAndStateNotAndPrivacyOrderByIdDesc("Pendiente",
+                "Rechazado", "Eliminado", "Público");
         return ResponseEntity.ok(reportsToShow);
     }
 
     @GetMapping(value = "{reportId}")
-    public Report getById(@PathVariable("reportId") int reportId){
-        return this.reportRepository.findById(reportId).orElseThrow(()->new ResourceNotFoundException("Error, el reporte no existe."));
+    public Report getById(@PathVariable("reportId") int reportId) {
+        return this.reportRepository.findById(reportId).orElseThrow(() -> new ResourceNotFoundException("Error, el reporte no existe."));
     }
 
     @GetMapping(value = "/byUserEmail/{email}")
     public ResponseEntity<List<Report>> getByUserEmail(@PathVariable("email") String email) throws Exception {
         User userFound = userRest.getByEmail(email).getBody();
         List<Report> userReports = reportRepository.findByUserOrderByIdDesc(userFound);
-        if(userReports.isEmpty()){
+        if (userReports.isEmpty()) {
             return new ResponseEntity(new Message("Este Usuario no ha Realizado Reportes", HttpStatus.NO_CONTENT.value()), HttpStatus.NO_CONTENT);
         }
         return ResponseEntity.ok(userReports);
     }
 
     @GetMapping(value = "/byMunicipality/{muni}")
-    public ResponseEntity<List<Report>> getByMunicipality(@PathVariable("muni")Municipality municipality){
+    public ResponseEntity<List<Report>> getByMunicipality(@PathVariable("muni") Municipality municipality) {
         List<Report> reportsMunicipality = reportRepository.findByMunicipalityOrderByIdDesc(municipality);
-        if(reportsMunicipality.isEmpty()){
-            return new ResponseEntity(new Message("Municipalidad no tiene reportes",HttpStatus.NO_CONTENT.value()), HttpStatus.NO_CONTENT);
+        if (reportsMunicipality.isEmpty()) {
+            return new ResponseEntity(new Message("Municipalidad no tiene reportes", HttpStatus.NO_CONTENT.value()), HttpStatus.NO_CONTENT);
         }
         return ResponseEntity.ok(reportsMunicipality);
     }
 
     @GetMapping(value = "/byState/{state}")
-    public ResponseEntity<List<Report>> getByState(@PathVariable("state") String state){
+    public ResponseEntity<List<Report>> getByState(@PathVariable("state") String state) {
         List<Report> reportsByState = reportRepository.findByStateOrderByIdDesc(state);
-        if(reportsByState.isEmpty()){
+        if (reportsByState.isEmpty()) {
             return new ResponseEntity(new Message("No Existen reportes con el estado: " + state, HttpStatus.NO_CONTENT.value()), HttpStatus.NO_CONTENT);
         }
         return ResponseEntity.ok(reportsByState);
@@ -86,51 +87,57 @@ public class ReportRest {
 
     @CrossOrigin
     @PostMapping(value = "/city/{cityName}")
-    public ResponseEntity<Report> create(@RequestBody Report report, @PathVariable("cityName") String cityName) throws  Exception{
+    public ResponseEntity<Report> create(@RequestBody Report report, @PathVariable("cityName") String cityName) throws Exception {
 
         Coordenates newCoordenates = report.getCoordenates();
         Coordenates savedCoordenates = coordenatesRest.create(newCoordenates).getBody();
         report.setCoordenates(savedCoordenates);
-        Municipality municipalityFound = cityRest.getMunicipalityByCityName(cityName).getBody();
-        report.setMunicipality(municipalityFound);
+        ResponseEntity<Municipality> municipalitySearch = cityRest.getMunicipalityByCityName(cityName);
 
-        report.setCreatedAt(new Date(System.currentTimeMillis()));
+        if (municipalitySearch.getStatusCodeValue() == HttpStatus.BAD_REQUEST.value())
+            return new ResponseEntity(new Message("No tenemos municipalidades que atiendan esta ciudad.", HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
+        else {
+            Municipality municipalityFound = municipalitySearch.getBody();
 
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
+            report.setMunicipality(municipalityFound);
 
-        Set<ConstraintViolation<Report>> violations = validator.validate(report);
-        for(ConstraintViolation<Report> violation : violations) {
-            return new ResponseEntity(new Message(violation.getMessage(),HttpStatus.UNPROCESSABLE_ENTITY.value()), HttpStatus.UNPROCESSABLE_ENTITY);
+            report.setCreatedAt(new Date(System.currentTimeMillis()));
+
+            ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+            Validator validator = factory.getValidator();
+
+            Set<ConstraintViolation<Report>> violations = validator.validate(report);
+            for (ConstraintViolation<Report> violation : violations) {
+                return new ResponseEntity(new Message(violation.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY.value()), HttpStatus.UNPROCESSABLE_ENTITY);
+            }
+            Report reportSaved = reportRepository.save(report);
+            if (reportSaved != null) {
+                return ResponseEntity.ok(reportSaved);
+            } else {
+                return new ResponseEntity(new Message("Error al crear reporte", HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
+            }
         }
-        Report reportSaved = reportRepository.save(report);
-        if( reportSaved!= null) {
-            return ResponseEntity.ok(reportSaved);
-        }else {
-            return new ResponseEntity(new Message("Error al crear reporte",HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
-        }
-
     }
 
     @CrossOrigin
     @PutMapping
-    public ResponseEntity update(@RequestBody Report reportChanges){
+    public ResponseEntity update(@RequestBody Report reportChanges) {
 
         Optional<Report> existingReport = reportRepository.findById(reportChanges.getId());
 
-        if(existingReport.isPresent()) {
+        if (existingReport.isPresent()) {
             Report updateReport = existingReport.get();
             updateReport.setDescription(reportChanges.getDescription());
             updateReport.setPrivacy(reportChanges.getPrivacy());
             updateReport.setState(reportChanges.getState());
 
-            if(reportRepository.save(updateReport) != null) {
-                return new ResponseEntity(new Message("Reporte actualizado exitosamente",HttpStatus.OK.value()), HttpStatus.OK);
-            }else {
-                return new ResponseEntity(new Message("Error al actualizar reporte",HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
+            if (reportRepository.save(updateReport) != null) {
+                return new ResponseEntity(new Message("Reporte actualizado exitosamente", HttpStatus.OK.value()), HttpStatus.OK);
+            } else {
+                return new ResponseEntity(new Message("Error al actualizar reporte", HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
             }
-        }else {
-            return new ResponseEntity(new Message("El reporte no existe",HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity(new Message("El reporte no existe", HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
         }
     }
 
