6565db8ab6f535151dad2229ae763880d6256728
JuanRuiz2406 <44301111+JuanRuiz2406@users.noreply.github.com>
2021-06-28 21:53:21 -0600

    Update ReportsMyCityApplicationTests.java

--- src/test/java/com/api/ReportsMyCity/ReportsMyCityApplicationTests.java
+++ src/test/java/com/api/ReportsMyCity/ReportsMyCityApplicationTests.java
@@ -1,13 +1,8 @@
 package com.api.ReportsMyCity;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ReportsMyCityApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
